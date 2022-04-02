1e7fa2d76cf9dd9d0a453512b796066f2bf7f991
jihm <jonathan.ihm@igor-tech.com>
2022-04-02 11:27:47 -0500

    converted to typescript and added initial template names

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Body from './components/Body'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <>
+        <div>Showcase</div>
+        <Header />
+        <Body />
+        <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <>
+        <div>Jonathan Ihm</div>
+      </>
+  )
+}
+
+export default Header
